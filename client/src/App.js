4b4798e9f6d5cc7e021b3c4622cfd54ebd71d9b8
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-04-24 12:19:31 -0400

    Added links to header and did a little styling, very little.

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import BubblePage from "./components/BubblePage";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import "./App.css";
 import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
@@ -12,16 +13,21 @@ function App() {
     <Router>
       <div className='sticky-top navBar text-dark'>
         <h1>Bubbles</h1>
-        {/* <ul className="links">
-          <li className="p-2">Login</li>
-          <li className="p-2">Bubbles</li>
-        </ul> */}
-        <Switch>
-          <Route exact path='/' component={Login} />
-          <PrivateRoute exact path='/protected' component={BubblePage} />
-          <Route component={Login} />
-        </Switch>
+        <ul className='links'>
+          <li className='p-2'>
+            <Link to='/login'>Login</Link>{" "}
+          </li>
+          <li className='p-2'>
+            <Link to='/protected'>Bubbles</Link>
+          </li>
+        </ul>
       </div>
+
+      <Switch>
+        <Route path='/login' component={Login} />
+        <PrivateRoute exact path='/protected' component={BubblePage} />
+        <Route component={Login} />
+      </Switch>
     </Router>
   );
 }
