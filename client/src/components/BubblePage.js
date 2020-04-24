df65b310037f9aa0a3f1ee4514f8b18b68edc5aa
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-04-24 12:38:28 -0400

    Get request done. Bubbles render over top of the edit and delete part of the form. I just have to write the functions to be able to edit and delete and then I will see about going back and styling but that should be enough to achieve MVP.

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -3,11 +3,24 @@ import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { axiosWithAuth } from "./utils/axiosAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("http://localhost:5000/api/colors")
+      .then((res) => {
+        console.log(res);
+        setColorList(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <>
