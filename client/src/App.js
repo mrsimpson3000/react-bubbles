8308e71659f2701f6510a32b7bd7582807a70d33
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-04-24 12:10:51 -0400

    It's not styled or anything but a form renders and you can login. Once logged in it immediately takes you to the BubblePage. Since styling is not part of the MVP requirements I want to make sure everything else works first. Then I will go back and put a little styling on the header and login form.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+import BubblePage from "./components/BubblePage";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+      <div className='sticky-top navBar text-dark'>
+        <h1>Bubbles</h1>
+        {/* <ul className="links">
+          <li className="p-2">Login</li>
+          <li className="p-2">Bubbles</li>
+        </ul> */}
+        <Switch>
+          <Route exact path='/' component={Login} />
+          <PrivateRoute exact path='/protected' component={BubblePage} />
+          <Route component={Login} />
+        </Switch>
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+import { axiosWithAuth } from "./utils/axiosAuth";
+
+export default function Login(props) {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    setCredentials({
+      ...credentials,
+      [event.target.name]: event.target.value,
+    });
+    console.log(event.target.value);
+  };
+
+  const login = (event) => {
+    event.preventDefault();
+
+    axiosWithAuth()
+      .post("http://localhost:5000/api/login", credentials)
+      .then((result) => {
+        console.log(result);
+        localStorage.setItem("token", result.data.payload);
+        console.log(props);
+        props.history.push("/protected");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form className='loginForm' onSubmit={login}>
+        <input
+          type='text'
+          name='username'
+          placeholder='username'
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          name='password'
+          placeholder='password'
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button>Log in</button>
+      </form>
     </>
   );
-};
-
-export default Login;
+}
